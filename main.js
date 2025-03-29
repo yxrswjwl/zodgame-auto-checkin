f046050abb592a5c46516317eb7df9fbe77d051e
ewigl <ewiglichtl@gmail.com>
2025-03-29 12:31:30 +0800

    revert changes

--- main.js
+++ main.js
@@ -6,8 +6,23 @@ const MOODS = ["kx", "ng", "ym", "wl", "nu", "ch", "fd", "yl", "shuai"];
 const MOODS_LENGTH = MOODS.length;
 
 const baseHeaders = {
-  ContentType: "application/x-www-form-urlencoded",
+  accept:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+  "accept-language": "zh-CN,zh-TW;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6",
+  "cache-control": "max-age=0",
+  "content-type": "application/x-www-form-urlencoded",
+  priority: "u=0, i",
+  "sec-ch-ua":
+    '"Not:A-Brand";v="24", "Chromium";v="134", "Google Chrome";v="134"',
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": '"Windows"',
+  "sec-fetch-dest": "iframe",
+  "sec-fetch-mode": "navigate",
+  "sec-fetch-site": "same-origin",
+  "sec-fetch-user": "?1",
+  "upgrade-insecure-requests": "1",
   Referer: "https://zodgame.xyz/plugin.php?id=dsu_paulsign:sign",
+  "Referrer-Policy": "strict-origin-when-cross-origin",
 };
 
 function checkIfSuccess(data) {
